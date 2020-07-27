b93a8f48fa83e7b972e00523a7ed220c9388cbb7
mirrorable1 <60344011+mirrorable1@users.noreply.github.com>
2020-07-27 16:10:51 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-
-
-echo "Please run as super user & disconnect Asus BT-400"
+echo "Please run as super user"
 sleep 5
 echo "Converting .hex to .hcd..."
 hex2hcd BCM20702A1_001.002.014.1443.1467.hex -o BCM20702A1-0b05-17cb.hcd
@@ -9,7 +7,12 @@ sleep 3
 
 echo "Making Directory & Installing Converted File..."
 sudo mkdir -p /lib/firmware/brcm/
-sudo cp BCM20702A1-0b05-17cb.hcd /usr/lib/firmware/brcm/BCM20702A1-0b05-17cb.hcd
-echo "Done installing Driver Should work now please plug in your Asus BT-400 Now"
+sudo cp BCM20702A1-0b05-17cb.hcd /lib/firmware/brcm/BCM20702A1-0b05-17cb.hcd
+echo "Done installing Driver"
+sleep 3
+echo "Installing pulse audio module"
+sudo apt-get install pulseaudio-module-bluetooth -y
+sleep 3
+echo "Reboot Computer And Bluetooth Should Now Work"
 sleep 10
 exit
